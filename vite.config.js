b85c6594546c8fbe8a9ecf3e4dcf60a54ff37951
ShoneYohannan <shoneyohannan3@gmail.com>
2026-08-24 21:15:14 +0530

    Organize repository root and add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Dazzling-Destinations/',
   plugins: [
     react(),
     tailwindcss()
